5c1ba71a7bb9970c32ead1566ccc5dfc46334df5
coolishsecv2 <coolishsecv2@users.noreply.github.com>
2026-09-19 00:13:04 +0800

    add x6711 config

---
+++ board/infinix/board-x6711.c
@@ -0,0 +1,14 @@
+//
+// SPDX-FileCopyrightText: 2026 coolishsecv2 <coolishsecv2@users.noreply.github.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for Infinix X6711\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for Infinix X6711\n");
+}
